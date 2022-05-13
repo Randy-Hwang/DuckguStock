7588c84a4fa52991819c510227c97eb770302441
Randy <99023957+Randy-Hwang@users.noreply.github.com>
2022-05-13 12:37:11 +0900

    update crawlling

--- js/news.js
+++ js/news.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 import cheerio from "cheerio";
 
-let searchKeyWord = encodeURI("삼성전자");
+const samsung = document.getElementById("samsung");
+const naver = document.getElementById("naver");
+const apple = document.getElementById("apple");
+const tesla = document.getElementById("tesla");
+const google = document.getElementById("google");
+const microsoft = document.getElementById("microsoft");
+const nvidia = document.getElementById("nvidia");
+
+let buttonWord = "삼성전자";
+let searchKeyWord = encodeURI(`${buttonWord}`);
+
+samsung.addEventListener("click", () => (buttonWord = "삼성전자"));
+naver.addEventListener("click", () => (buttonWord = "naver 네이버"));
+apple.addEventListener("click", () => (buttonWord = "애플"));
+tesla.addEventListener("click", () => (buttonWord = "테슬라"));
+google.addEventListener("click", () => (buttonWord = "구글"));
+microsoft.addEventListener("click", () => (buttonWord = "마이크로소프트"));
+nvidia.addEventListener("click", () => (buttonWord = "엔비디아"));
+
 // axios를 활용해 AJAX로 HTML 문서를 가져오는 함수 구현
 async function getHTML() {
   try {
     return await axios.get(
-      `https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=${searchKeyWord}`
+      `https://cors-anywhere.herokuapp.com/https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=${searchKeyWord}`
     );
   } catch (error) {
     console.error(error);
